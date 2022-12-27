4abfe810dd8f5124e34b1739f4b64014b1c390b1
Elbarae El-ghannouchi <barae.gh@gmail.com>
2022-12-27 20:23:35 +0100

    setup docker-compose file

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,10 +1,10 @@
-
+#!/bin/bash
 service mysql start
 
 # Creating a Database and User for WordPress
 mariadb -e "CREATE DATABASE mariadb DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
 
-mariadb -e "GRANT ALL ON mariadb.* TO 'barae'@'localhost' IDENTIFIED BY '1234';"
+mariadb -e "GRANT ALL ON mariadb.* TO 'barae'@'%' IDENTIFIED BY '1234';"
 
 mariadb -e "FLUSH PRIVILEGES;"
 

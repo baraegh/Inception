464ecf76097a763e1dea5f548e5e1f78f8ced77e
baraegh <barae.gh@gmail.com>
2022-12-28 01:22:23 +0100

    fix mariadb container error when I run it using docker-compose file

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -6,6 +6,6 @@ mysql -u root -e "CREATE DATABASE mariadb;"
 
 mysql -u root -e "GRANT ALL ON mariadb.* TO 'barae'@'%' IDENTIFIED BY '1234';"
 
-mysql -u root  -e "FLUSH PRIVILEGES;"
+mysql -u root -e "FLUSH PRIVILEGES;"
 
 # service mysql stop    

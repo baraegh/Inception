8cf4b4a42b9d97b048e536ff20bee6a4f13685b4
baraegh <barae.gh@gmail.com>
2022-12-31 01:03:47 +0100

    add args and env variables to mariadb dockerfile and the script

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,10 +2,14 @@
 service mysql start
 
 # Creating a Database and User for WordPress
-mysql -u root -e "CREATE DATABASE mariadb;"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 
-mysql -u root -e "GRANT ALL ON mariadb.* TO 'barae'@'%' IDENTIFIED BY '1234';"
+mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'"
+
+mysql -u root -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%';"
 
 mysql -u root -e "FLUSH PRIVILEGES;"
 
-# service mysql stop    
+# set password for the root user
+
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';"

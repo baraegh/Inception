e7620fed0037cdf819e31dc2385663b5e0fcbdef
baraegh <barae.gh@gmail.com>
2022-12-31 01:06:27 +0100

    rm data contents

--- srcs/data/wp/script.sh
+++
@@ -1,8 +0,0 @@
-
-# configure wordpress usinf WP-CLI
-wp core config --dbhost=mariadb --dbname=mariadb --dbuser=barae --dbpass=1234 --allow-root --skip-check
-
-wp core install --url=test.com --title="test" --admin_name=barae --admin_password=1234 --admin_email=test@mail.com --allow-root --skip-check
-
-# To delete history
-# history -c

a4e4100e5ae9c907019bf9f84d9327bd452a720d
baraegh <barae.gh@gmail.com>
2023-01-05 22:06:17 +0100

    setup files permessions

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -3,8 +3,6 @@ echo "listen = 9000" >> /etc/php/7.3/fpm/pool.d/www.conf
 if ! wp core is-installed --allow-root; then
     wp core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_EMAIL} --skip-email --allow-root
     wp user create ${WP_USER} ${WP_USER_EMAIL} --role=${WP_USER_ROLE} --display_name=${WP_USER} --user_pass=${WP_USER_PASS} --allow-root
-    wp config set WP_REDIS_HOST redis --type=constant --allow-root
-    # wp config set WP_REDIS_PORT 6379 --type=constant --allow-root
     # wp config set WP_REDIS_TIMEOUT 1 --type=constant --allow-root
     # wp config set WP_REDIS_READ_TIMEOUT 1 --type=constant --allow-root
     # wp config set WP_REDIS_DATABASE 0 --type=constant --allow-root
@@ -21,7 +19,12 @@ if ! wp plugin is-active redis-cache --allow-root; then
     wp plugin activate redis-cache --allow-root
 fi
 
+wp config set WP_REDIS_HOST redis --type=constant --allow-root
+wp config set WP_REDIS_PORT 6379 --type=constant --allow-root
+
 wp redis enable --allow-root
+
+chown -R www-data:1000 /var/www/html/wordpress/*
 
 service php7.3-fpm start
 service php7.3-fpm stop
